e4728024c8194edc51eca734308816e4eaa5fe9a
Thiru <tp60271@users.noreply.github.com>
2020-06-21 00:27:16 +0800

    Update main.tf

--- security_controls_scp/main.tf
+++ security_controls_scp/main.tf
@@ -9,35 +9,35 @@ module "account" {
 }
 
 ## Deploy AWS Config AWS Org SCPs
-module "awsconfig" {
-  source = "./modules/awsconfig"
+#module "awsconfig" {
+#  source = "./modules/awsconfig"
 
-  target_id = var.target_id
-}
+#  target_id = var.target_id
+#}
 
 ## Deploy CloudTrail AWS Org SCPs
-module "cloudtrail" {
-  source = "./modules/cloudtrail"
+#module "cloudtrail" {
+#  source = "./modules/cloudtrail"
 
-  target_id = var.target_id
-}
+#  target_id = var.target_id
+#}
 
 ## Deploy EC2 AWS Org SCPs
-module "ec2" {
-  source = "./modules/ec2"
+#module "ec2" {
+#  source = "./modules/ec2"
 
-  target_id         = var.target_id
-  ami_creator_account = var.ami_creator_account
-  ami_tag_key       = var.ami_tag_key
-  ami_tag_value     = var.ami_tag_value
-}
+#  target_id         = var.target_id
+#  ami_creator_account = var.ami_creator_account
+#  ami_tag_key       = var.ami_tag_key
+#  ami_tag_value     = var.ami_tag_value
+#}
 
 ## Deploy GuardDuty AWS Org SCPs
-module "guardduty" {
-  source = "./modules/guardduty"
+#module "guardduty" {
+#  source = "./modules/guardduty"
 
-  target_id = var.target_id
-}
+#  target_id = var.target_id
+#}
 
 ## Deploy IAM Aws Org SCPs
 module "iam" {
@@ -62,16 +62,16 @@ module "s3" {
 }
 
 ## Deploy Shield AWS Org SCPs
-module "shield" {
-  source = "./modules/shield"
+#module "shield" {
+#  source = "./modules/shield"
 
-  target_id = var.target_id
-}
+#  target_id = var.target_id
+#}
 
 ## Deploy VPC AWS Org SCPs
-module "vpc" {
-  source = "./modules/vpc"
+#module "vpc" {
+#  source = "./modules/vpc"
 
-  target_id = var.target_id
-}
+#  target_id = var.target_id
+#}
 
